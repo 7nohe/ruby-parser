63e631f848626fef1c073cafb1d50866dcbe2c9f
Urata Daiki <7nohe@users.noreply.github.com>
2024-04-08 08:32:09 +0900

    Add support for building on macOS with Apple Silicon

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ switch (platform) {
           if (localFileExisted) {
             nativeBinding = require('./ruby-parser.android-arm64.node')
           } else {
-            nativeBinding = require('ruby-parser-android-arm64')
+            nativeBinding = require('@7nohe/ruby-parser-android-arm64')
           }
         } catch (e) {
           loadError = e
@@ -49,7 +49,7 @@ switch (platform) {
           if (localFileExisted) {
             nativeBinding = require('./ruby-parser.android-arm-eabi.node')
           } else {
-            nativeBinding = require('ruby-parser-android-arm-eabi')
+            nativeBinding = require('@7nohe/ruby-parser-android-arm-eabi')
           }
         } catch (e) {
           loadError = e
@@ -69,7 +69,7 @@ switch (platform) {
           if (localFileExisted) {
             nativeBinding = require('./ruby-parser.win32-x64-msvc.node')
           } else {
-            nativeBinding = require('ruby-parser-win32-x64-msvc')
+            nativeBinding = require('@7nohe/ruby-parser-win32-x64-msvc')
           }
         } catch (e) {
           loadError = e
@@ -83,7 +83,7 @@ switch (platform) {
           if (localFileExisted) {
             nativeBinding = require('./ruby-parser.win32-ia32-msvc.node')
           } else {
-            nativeBinding = require('ruby-parser-win32-ia32-msvc')
+            nativeBinding = require('@7nohe/ruby-parser-win32-ia32-msvc')
           }
         } catch (e) {
           loadError = e
@@ -97,7 +97,7 @@ switch (platform) {
           if (localFileExisted) {
             nativeBinding = require('./ruby-parser.win32-arm64-msvc.node')
           } else {
-            nativeBinding = require('ruby-parser-win32-arm64-msvc')
+            nativeBinding = require('@7nohe/ruby-parser-win32-arm64-msvc')
           }
         } catch (e) {
           loadError = e
@@ -113,7 +113,7 @@ switch (platform) {
       if (localFileExisted) {
         nativeBinding = require('./ruby-parser.darwin-universal.node')
       } else {
-        nativeBinding = require('ruby-parser-darwin-universal')
+        nativeBinding = require('@7nohe/ruby-parser-darwin-universal')
       }
       break
     } catch {}
@@ -124,7 +124,7 @@ switch (platform) {
           if (localFileExisted) {
             nativeBinding = require('./ruby-parser.darwin-x64.node')
           } else {
-            nativeBinding = require('ruby-parser-darwin-x64')
+            nativeBinding = require('@7nohe/ruby-parser-darwin-x64')
           }
         } catch (e) {
           loadError = e
@@ -138,7 +138,7 @@ switch (platform) {
           if (localFileExisted) {
             nativeBinding = require('./ruby-parser.darwin-arm64.node')
           } else {
-            nativeBinding = require('ruby-parser-darwin-arm64')
+            nativeBinding = require('@7nohe/ruby-parser-darwin-arm64')
           }
         } catch (e) {
           loadError = e
@@ -157,7 +157,7 @@ switch (platform) {
       if (localFileExisted) {
         nativeBinding = require('./ruby-parser.freebsd-x64.node')
       } else {
-        nativeBinding = require('ruby-parser-freebsd-x64')
+        nativeBinding = require('@7nohe/ruby-parser-freebsd-x64')
       }
     } catch (e) {
       loadError = e
@@ -174,7 +174,7 @@ switch (platform) {
             if (localFileExisted) {
               nativeBinding = require('./ruby-parser.linux-x64-musl.node')
             } else {
-              nativeBinding = require('ruby-parser-linux-x64-musl')
+              nativeBinding = require('@7nohe/ruby-parser-linux-x64-musl')
             }
           } catch (e) {
             loadError = e
@@ -187,7 +187,7 @@ switch (platform) {
             if (localFileExisted) {
               nativeBinding = require('./ruby-parser.linux-x64-gnu.node')
             } else {
-              nativeBinding = require('ruby-parser-linux-x64-gnu')
+              nativeBinding = require('@7nohe/ruby-parser-linux-x64-gnu')
             }
           } catch (e) {
             loadError = e
@@ -203,7 +203,7 @@ switch (platform) {
             if (localFileExisted) {
               nativeBinding = require('./ruby-parser.linux-arm64-musl.node')
             } else {
-              nativeBinding = require('ruby-parser-linux-arm64-musl')
+              nativeBinding = require('@7nohe/ruby-parser-linux-arm64-musl')
             }
           } catch (e) {
             loadError = e
@@ -216,7 +216,7 @@ switch (platform) {
             if (localFileExisted) {
               nativeBinding = require('./ruby-parser.linux-arm64-gnu.node')
             } else {
-              nativeBinding = require('ruby-parser-linux-arm64-gnu')
+              nativeBinding = require('@7nohe/ruby-parser-linux-arm64-gnu')
             }
           } catch (e) {
             loadError = e
@@ -231,7 +231,7 @@ switch (platform) {
           if (localFileExisted) {
             nativeBinding = require('./ruby-parser.linux-arm-gnueabihf.node')
           } else {
-            nativeBinding = require('ruby-parser-linux-arm-gnueabihf')
+            nativeBinding = require('@7nohe/ruby-parser-linux-arm-gnueabihf')
           }
         } catch (e) {
           loadError = e
@@ -246,7 +246,7 @@ switch (platform) {
             if (localFileExisted) {
               nativeBinding = require('./ruby-parser.linux-riscv64-musl.node')
             } else {
-              nativeBinding = require('ruby-parser-linux-riscv64-musl')
+              nativeBinding = require('@7nohe/ruby-parser-linux-riscv64-musl')
             }
           } catch (e) {
             loadError = e
@@ -259,7 +259,7 @@ switch (platform) {
             if (localFileExisted) {
               nativeBinding = require('./ruby-parser.linux-riscv64-gnu.node')
             } else {
-              nativeBinding = require('ruby-parser-linux-riscv64-gnu')
+              nativeBinding = require('@7nohe/ruby-parser-linux-riscv64-gnu')
             }
           } catch (e) {
             loadError = e
@@ -274,7 +274,7 @@ switch (platform) {
           if (localFileExisted) {
             nativeBinding = require('./ruby-parser.linux-s390x-gnu.node')
           } else {
-            nativeBinding = require('ruby-parser-linux-s390x-gnu')
+            nativeBinding = require('@7nohe/ruby-parser-linux-s390x-gnu')
           }
         } catch (e) {
           loadError = e
---
+++ npm/darwin-arm64/README.md
@@ -0,0 +1,3 @@
+# `@7nohe/ruby-parser-darwin-arm64`
+
+This is the **aarch64-apple-darwin** binary for `@7nohe/ruby-parser`
